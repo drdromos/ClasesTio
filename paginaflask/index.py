2af827f513826a8a1ad70f102d7cc989bb735262
drdromos <alediaza2004@gmail.com>
2022-10-07 18:17:37 -0300

    se agrego detalle.html

--- paginaflask/index.py
+++ paginaflask/index.py
@@ -15,11 +15,15 @@ def signup():
         fullname = request.form['fullname']
         celular = request.form['phone']
         edad = request.form['age']
-        return redirect(url_for('/'))
         return render_template('signup.html' , nombre=fullname , telefono=celular , vejez=edad)
     else:
         return "algo está mal"
     return render_template('signup.html')
+
+@app.route("/detalle/<string:name>/<int:age>" , methods=["GET"])
+def detalle(name:str , age:int):
+    if request.method == "GET":
+        return render_template('detalle.html' , nombre=name , edad=age)
 
 if __name__ == "__main__":   
     app.run(host='0.0.0.0')
